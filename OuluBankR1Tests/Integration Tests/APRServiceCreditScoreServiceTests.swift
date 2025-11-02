25cebe9fdcf75e9635b951a656f792bf11932572
Adam Cegiełka <adamcegielka@tlen.pl>
2025-11-02 15:37:58 +0100

    test: added integration test

---
+++ OuluBankR1Tests/Integration Tests/APRServiceCreditScoreServiceTests.swift
@@ -0,0 +1,35 @@
+//
+//  APRServiceCreditScoreServiceTests.swift
+//  OuluBankR1Tests
+//
+//  Updated by Adam Cegielka on 02/11/2025.
+//
+
+import Testing
+@testable import OuluBankR1
+
+struct APRServiceCreditScoreServiceTests {
+    
+    // APR service should call get credit score on credit score service
+    @Test
+    func apr_service_calls_get_credit_score_on_credit_score_service() async throws {
+        
+        let validSSN = "123-45-6789"
+        
+        var mockCreditScoreService = MockCreditScoreService()
+        
+        try await confirmation("APRService did not call getCreditScore on mockCreditScoreService", expectedCount: 1) { confirmation in
+            // need to make sure that mockCreditScoreService.getCreditScore is fired
+            
+            mockCreditScoreService.onGetCreditScore = { ssn in
+            
+                confirmation()
+                return CreditScore(score: 500, lastUpdated: "11/02/2025", reportedBy: "Experian")
+            }
+            
+            let aprService = APRService(creditScoreService: mockCreditScoreService)
+            let _ = try await aprService.getAPR(ssn: validSSN)
+        }
+    }
+
+}
--- OuluBankR1Tests/Mocks/MockCreditScoreService.swift
+++ OuluBankR1Tests/Mocks/MockCreditScoreService.swift
@@ -10,16 +10,24 @@ import Foundation
 
 struct MockCreditScoreService: CreditScoreServiceProtocol {
     
+    var onGetCreditScore: ((String) -> CreditScore?)?
+    
     func getCreditScore(ssn: String) async throws -> CreditScore? {
-        switch ssn {
-        case "123-45-6789": // good credit score
-            return CreditScore(score: 720, lastUpdated: "02/11/2025", reportedBy: "Experian")
-        case "987-65-4321": // bad credit score
-            return CreditScore(score: 540, lastUpdated: "01/11/2025", reportedBy: "Experian")
-        case "111-22-3333":
-            return CreditScore(score: nil, lastUpdated: "01/11/2025", reportedBy: "Experian")
-        default: // no credit score
-            return nil
+        
+        if let onGetCreditScore {
+            return onGetCreditScore(ssn)
+        } else {
+            
+            switch ssn {
+            case "123-45-6789": // good credit score
+                return CreditScore(score: 720, lastUpdated: "02/11/2025", reportedBy: "Experian")
+            case "987-65-4321": // bad credit score
+                return CreditScore(score: 540, lastUpdated: "01/11/2025", reportedBy: "Experian")
+            case "111-22-3333":
+                return CreditScore(score: nil, lastUpdated: "01/11/2025", reportedBy: "Experian")
+            default: // no credit score
+                return nil
+            }
         }
     }
 }
