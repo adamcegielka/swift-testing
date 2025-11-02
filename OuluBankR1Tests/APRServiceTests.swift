52c136f056ae7e3bf8fa26cf191cbcff6134a6d4
Adam Cegiełka <adamcegielka@tlen.pl>
2025-11-02 13:43:23 +0100

    test: add APR test

--- OuluBankR1Tests/APRServiceTests.swift
+++ OuluBankR1Tests/APRServiceTests.swift
@@ -3,6 +3,7 @@
 //  OuluBankR1Tests
 //
 //  Created by Mohammad Azam on 2/15/25.
+//  Updated by Adam on 11/2/2025.
 //
 
 import Testing
@@ -10,6 +11,13 @@ import Testing
 
 struct APRServiceTests {
     
-
-    
+    // APR is calculated within expected range for valid SSN
+    @Test
+    func apr_is_wthin_expected_range_for_valid_ssn() async throws {
+        let ssn: String = "123-45-6789" // ssn with credit score > 650
+        let expectedAPRRange = 1.0...3.0
+        let aprService = APRService(creditScoreService: CreditScoreService())
+        let apr = try await aprService.getAPR(ssn: ssn)
+        #expect(expectedAPRRange.contains(apr))
+    }    
 }
