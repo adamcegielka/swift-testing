c815171e142707a7b8cf37da16c5d134b7db4b61
Adam Cegiełka <adamcegielka@tlen.pl>
2025-11-02 13:41:21 +0100

    chore: add getAPR  function

--- OuluBankR1/Services/APRService.swift
+++ OuluBankR1/Services/APRService.swift
@@ -3,10 +3,46 @@
 //  OuluBankR1
 //
 //  Created by Mohammad Azam on 2/15/25.
+//  Updated by Adam on 11/2/2025.
 //
 
 import Foundation
 
+enum CreditScoreServiceError: LocalizedError {
+    case noCreditScoreFound
+    
+    // Provide localized error descriptions
+    var errorDescription: String? {
+        switch self {
+        case .noCreditScoreFound:
+            return NSLocalizedString(
+                "No credit score found for the provided SSN",
+                comment: "Error message when no credit score is found"
+            )
+        }
+    }
+}
 
+struct APRService {
+    
+    // third part service
+    let creditScoreService: CreditScoreService
+    
+    func getAPR(ssn: String) async throws -> Double {
+        guard let creditScore = try await creditScoreService.getCreditScore(ssn: ssn) else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+        
+        if let score = creditScore.score {
+            if score > 650 {
+                return Double.random(in: 1...3)
+            } else {
+                return Double.random(in: 6...10)
+            }
+        } else {
+            throw CreditScoreServiceError.noCreditScoreFound
+        }
+    }
+}
 
 
